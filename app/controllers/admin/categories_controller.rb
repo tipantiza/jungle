621ddffa7c8c5fd709365993dc94dd634251243b
landon tipantiza <landontipantiza@gmail.com>
2020-07-23 19:19:50 +0000

    rough html of the /categories and /categories/new

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,8 +4,16 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Product.new
   end
 
   def create
+    @category = Product.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 end
