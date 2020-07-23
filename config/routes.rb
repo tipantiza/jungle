220a18a8e6539305ea4496879ec70111811e3c85
landon tipantiza <landontipantiza@gmail.com>
2020-07-23 19:12:16 +0000

    created routes for admin/categories and created a category controller. briefly started to create html for the index page

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
  
+  
   get '/about' => 'about#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
